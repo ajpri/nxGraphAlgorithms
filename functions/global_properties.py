3dbe84bf0a1a5c8ec0bf761c5b188c0e0f05abbe
Yerusalem <salita20@yahoo.com>
2019-04-24 15:50:43 -0500

    Update global_properties.py

--- functions/global_properties.py
+++ functions/global_properties.py
@@ -1,38 +1,46 @@
 import networkx as nx
 from functions.local_properties import neighbors
 
-
+#Returns list of vertices
 def V(G):
-    return nx.nodes(G)  #vertices
+    return nx.nodes(G) 
 
+#Returns list of edges
 def E(G):
-    return nx.edges(G)  #edges
+    return nx.edges(G)  
 
+#Returns number of Verticies
 def n(G):
-    return len(V(G))    #number of vertices (order)
+    return len(V(G)) 
 
+#number of edges (size)
 def m(G):
-    return len(E(G))    #number of edges (size)
+    return len(E(G))    
 
 
 #############################################################################
 
+#returns the length of neighbors
 def degree(G,v):
     return len(neighbors(G,v))
 
 #print(neighbors(G, '5'))
-    
+
+#returns the degree_sequence
 def degree_sequence(G):
     D = [degree(G,v) for v in V(G)]
     D.sort(reverse = True)
     return D
 
+#returns the maximum degree
 def maxmimum_degree(G):
     return degree_sequence(G)[0]
 
+#returns the minimum degree
 def minimum_degree(G):
     return degree_sequence(G)[-1]
 
+#returns the average of the degree
 def avg_degree(G):
     return sum(degree_sequence(G)) / n(G)
 

49ab4a1a01b8cb5efe3e5ec8fdb6bb2fcfb15378
Austin Pritchett <ajpri1998@gmail.com>
2019-04-24 08:37:16 -0500

    Cleanup
    Cleanup Repo

--- clique.py
+++ clique.py
@@ -3,25 +3,10 @@
 import networkx as nx
 #from functions.global_properties import V
 
-G = nx.read_edgelist('G.txt')
-nodes = G.nodes
-#print(V(G))
 
-
-
-
-#def is_clique(G, clique_nodes):
-   # for x in clique_nodes:
-        #for y in clique_nodes:
-            #if x!= y:
-                #if not(G.has_edge(x,y)):
-                    #return False
-    #return True
-    
     
 def is_clique(G, nodes):
     for x in nodes(nodes, 2):
         if x[1] not in G[x[0]]:
             return False
     return True
-#print(is_clique(G,nodes))

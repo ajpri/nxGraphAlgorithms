c5558a523d92dab8323fe429acf4f663fe57e7ce
Austin Pritchett <ajpri1998@gmail.com>
2019-04-24 08:56:13 -0500

    Greedy Coloring Havel Hakimi
    Added Greedy Coloring and Havel Hakimi, refactored to meet class SOPs

--- Havel_Hakimi.py
+++ Havel_Hakimi.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  8 16:17:36 2019
 
-@author: Shadow
-"""
+
+
+def Havel_hakimi_dervate(L):
+    d_1 = L[0]
+    L.pop(0)
+    for i in range(d_1):
+        L[i] -=1
+    L.sort(reverse=True)
+    return None
 
--- greedy_coloring.py
+++ greedy_coloring.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  8 16:17:36 2019
+import networkx as nx
+from  functions.global_properties import V
+from functions.local_properties import neighbors
+import itertools as it
 
-@author: Shadow
-"""
 
+def greedy_coloring (G):
+    colors = {}
+    for u in V(G):
+        neighbour_colors = {colors[v] for v in G[u] if v in colors}
+        for color in it.count():
+            if color not in neighbour_colors:
+                break
+        colors[u] = color
+    return colors

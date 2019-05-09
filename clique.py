ff5eb4ce289b90863dcdc3126559732670fd191a
Yerusalem <salita20@yahoo.com>
2019-05-09 18:55:00 -0500

    Update clique.py

--- clique.py
+++ clique.py
@@ -1,20 +1,49 @@
 import networkx as nx
-
 from itertools import *
-
 from functions.global_properties import V
-
 from functions.local_properties import neighbors
-
 G = nx.read_edgelist('graph_library/pan.txt')
 
+"""Returns True if every two distinct vertices are adjacent
 
+Parameters
+----------
+
+G:
+    A Graph containg vertices and edges contained in Graph G
+    
+S:
+    
+Returns 
+-------
+
+Bool
+        Returns True if every two distinct vertices are adjacent 
+     
+"""
 def is_clique(G,S): #set of vertices where every pair in the set forms an edge
     for v in S:
         if list(set(S)&set(neighbors(G,v))) != []:  #[] <-- empty list
             return False
     
     return True
+
+"""Returns True if every two distinct vertices are adjacent
+
+Parameters
+----------
+
+G:
+    A Graph containg vertices and edges contained in Graph G
+    
+S:
+    
+Returns 
+-------
+x
+        The maximum clique of the Graph
+
+"""
 
 def largest_clique_set(G):
     n = len(V(G))

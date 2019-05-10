b6bbcfb3f279f8105bda064bc80c7460560d91b9
Yerusalem <salita20@yahoo.com>
2019-05-09 21:10:55 -0500

    Update greedy_coloring.py

--- greedy_coloring.py
+++ greedy_coloring.py
@@ -3,20 +3,21 @@ from  functions.global_properties import V
 from functions.local_properties import neighbors
 import itertools as it
 
-"""Returns vertices with their corresponding colors
 
-Parameters
-----------
-G: int
+def greedy_coloring (G):
+   """Returns vertices with their corresponding colors
+
+   Parameters
+   ----------
+   G: int
    A graph containing the vertices and edges contained in graph G
    
-Returns
--------
-colors 
-    Returns vertices with their corresponding colors
-"""
-def greedy_coloring (G):
-    colors = {}
+   Returns
+   -------
+   colors 
+         Returns vertices with their corresponding colors
+   """    
+   colors = {}
     for u in V(G):
         neighbour_colors = {colors[v] for v in G[u] if v in colors}
         for color in it.count():
